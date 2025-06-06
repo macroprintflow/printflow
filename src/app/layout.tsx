5e3eb279579f76a7ae1cd7549dcf0fb4b88b1907
Sunil Verma <sunilmacro749@gmail.com>
2025-06-06 21:44:38 +0000

    the background is still in lgiht theme. Make the backgournd dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
@@ -20,7 +21,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased" suppressHydrationWarning={true}>
+      <body className="font-body antialiased bg-background text-foreground" suppressHydrationWarning={true}>
         {children}
         <Toaster />
       </body>
