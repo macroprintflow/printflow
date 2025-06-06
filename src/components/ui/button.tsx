dabc6870109d64d1e00516bfe4bb0a54bc6a001f
Sunil Verma <sunilmacro749@gmail.com>
2025-06-06 21:59:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -45,7 +45,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size }), className)}
         ref={ref}
         {...props}
       />
@@ -55,3 +55,4 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 Button.displayName = "Button"
 
 export { Button, buttonVariants }
+
